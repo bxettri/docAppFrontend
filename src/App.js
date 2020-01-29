17297c90f8bfc966fed3ad8bee9d31dc3b6c7cae
bxettri <bvekxettri@gmail.com>
2020-01-29 14:11:03 +0545

    Footer design with css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <div>
     <Login/>
+    <Footer/>
+    </div>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+
+                    {/* Column 1 */}
+
+                    <div className="col-md-3 col-sm-6">
+                   <ul>
+                       <li>demo demo</li>
+                       <li>demo demo</li>
+                       <li>demo demo</li>
+                       <li>demo demo</li>
+                       </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
